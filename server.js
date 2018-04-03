0767b7ce631068ef747dea6a545c48d75dea592a
JunruJim <lebenbot@gmail.com>
2018-04-03 16:33:29 -0700

    xu deploy on heroku

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-
 // Get the dependencies
+
 const express = require('express');
-
-// parse info when receive
-// parse JSON file from HTTP response
-const bodyParser = require('body-parser');
-const app = express();
-
 const path = require('path');
 const http = require('http');
+const bodyParser = require('body-parser');
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
+
 // Point static path to dist -- For building -- REMOVE
-// enclose these directories to be public
-// app.use(express.static(path.join(__dirname, 'dist')));
-// app.use(express.static(path.join(__dirname, 'src/assets')));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
 
 // CORS
 app.use(function(req, res, next) {
@@ -26,25 +25,25 @@ app.use(function(req, res, next) {
   next();
 });
 
+
+
+
 const port = process.env.PORT || '3100';
 app.set('port', port);
+
 
 // Create HTTP server
 const server = http.createServer(app);
 
-// var hello = require('./hello');
-// hello(app);
-// or require('./hello')(app);
+//var serverSide = require("./server/test-mongodb/app");
+//serverSide(app);
 
-require("./assignment/app")(app);
+
 
 // For Build: Catch all other routes and return the index file -- BUILDING
-// app.get('*', function (req, res) {
-  // res.sendFile(path.join(__dirname, 'dist/index.html'));
-//   res.sendFile(path.join(__dirname, 'dist/index.html'));
-// });
-
-server.listen( port , function() {
-  console.log('Node app is running on port', app.get('port'))
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
+
+server.listen( port , () => console.log('Running'));
