061cbf19c1d3c15d7454d1f63bcb503c10628480
XenomorphZHANG <chrismzyy@hotmail.com>
2018-04-17 22:21:29 -0700

    user and home page and faq done

---
+++ src/app/views/rst/rst-search-result/rst-search-result.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rst-search-result',
+  templateUrl: './rst-search-result.component.html',
+  styleUrls: ['./rst-search-result.component.css']
+})
+export class RstSearchResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
